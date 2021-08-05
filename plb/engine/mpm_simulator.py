[BugFix]: Fix bug for taichi_layer gradient and state setting precedure

plb/engine/mpm_simulator.py
@@ -305,30 +305,6 @@ class MPMSimulator:
                     C_grad[i,j,k] = self.C.grad[f,i][j,k]
 
     @ti.kernel
-    def readprimitives_grad(self,
-                            f:ti.i32,
-                            p_grad: ti.ext_arr(),
-                            v_grad:ti.ext_arr(),
-                            r_grad:ti.ext_arr(),
-                            w_grad:ti.ext_arr()):
-        for i in range(self.n_primitive):
-            p_grad[i,0] = self.primitives[i].position.grad[0]
-            p_grad[i,1] = self.primitives[i].position.grad[1]
-            p_grad[i,2] = self.primitives[i].position.grad[2]
-            v_grad[i,0] = self.primitives[i].v.grad[0]
-            v_grad[i,1] = self.primitives[i].v.grad[1]
-            v_grad[i,2] = self.primitives[i].v.grad[2]
-            r_grad[i,0] = self.primitives[i].rotation.grad[0]
-            r_grad[i,1] = self.primitives[i].rotation.grad[1]
-            r_grad[i,2] = self.primitives[i].rotation.grad[2]
-            r_grad[i,3] = self.primitives[i].rotation.grad[3]
-            w_grad[i,0] = self.primitives[i].w.grad[0]
-            w_grad[i,1] = self.primitives[i].w.grad[1]
-            w_grad[i,2] = self.primitives[i].w.grad[2]
-
-
-
-    @ti.kernel
     def setframe(self, f:ti.i32, x: ti.ext_arr(), v: ti.ext_arr(), F: ti.ext_arr(), C: ti.ext_arr()):
         for i in range(self.n_particles):
             for j in ti.static(range(self.dim)):
@@ -352,30 +328,6 @@ class MPMSimulator:
                 for k in ti.static(range(self.dim)):
                     self.F.grad[f,i][j,k] = F_grad[i,j,k]
                     self.C.grad[f,i][j,k] = C_grad[i,j,k]
-
-    @ti.kernel
-    def setkernel_grad(
-        self,
-        f:ti.i32,
-        p_grad: ti.ext_arr(),
-        v_grad: ti.ext_arr(),
-        r_grad: ti.ext_arr(),
-        w_grad: ti.ext_arr()):
-        for i in range(self.n_primitives):
-            self.primitives[i].position.grad[0] = p_grad[i,0]
-            self.primitives[i].position.grad[1] = p_grad[i,1]
-            self.primitives[i].position.grad[2] = p_grad[i,2]
-            self.primitives[i].v.grad[0] = v_grad[i,0]
-            self.primitives[i].v.grad[1] = v_grad[i,1]
-            self.primitives[i].v.grad[2] = v_grad[i,2]
-            self.primitives[i].rotation.grad[0] = r_grad[i,0]
-            self.primitives[i].rotation.grad[1] = r_grad[i,1]
-            self.primitives[i].rotation.grad[2] = r_grad[i,2]
-            self.primitives[i].rotation.grad[3] = r_grad[i,3]
-            self.primitives[i].w.grad[0] = w_grad[i,0]
-            self.primitives[i].w.grad[1] = w_grad[i,1]
-            self.primitives[i].w.grad[2] = w_grad[i,2]
-
 
     @ti.kernel
     def copyframe(self, source: ti.i32, target: ti.i32):
@@ -408,19 +360,26 @@ class MPMSimulator:
         v_grad = np.zeros((self.n_particles,self.dim), dtype = np.float64)
         F_grad = np.zeros((self.n_particles,self.dim,self.dim),dtype = np.float64)
         C_grad = np.zeros((self.n_particles,self.dim,self.dim),dtype = np.float64)
-        primitive_pos_grad = np.zeros((self.n_primitive,self.dim), dtype = np.float64)
-        primitive_rot_grad = np.zeros((self.n_primitive,self.dim+1), dtype = np.float64)
         self.readframe_grad(f,x_grad,v_grad,F_grad,C_grad)
-        self.readprimitives_grad(f,primitive_pos_grad,primitive_rot_grad)
-        for i,primitive in enumerate(self.primitives):
-            primitive_pos_grad[i] = primitive.get_pos_grad()
-            primitive_rot_grad[i] = primitive.get_rot_grad()
-        return x_grad, v_grad, primitive_pos_grad, primitive_rot_grad
+        out = [x_grad,v_grad,F_grad,C_grad]
+        for i in self.primitives:
+            out.append(i.get_state_grad(f))
+        return out
+
+    def get_current_state_grad(self):
+        return self.get_state_grad(self.cur)
 
     def set_state(self, f, state):
         self.setframe(f, *state[:4])
         for s, i in zip(state[4:], self.primitives):
             i.set_state(f, s)
+
+    # SImilar to state state grad is a list: 
+    # [x_grad, v_grad, F_grad, C_grad, p1_state_grad,..,pn_state_grad]
+    def set_state_grad(self,f, state_grad):
+        self.setframe_grad(f,*state_grad[:4])
+        for sg, i in zip(state_grad[4:],self.primitives):
+            i.set_state_grad(f,sg)
 
     @ti.kernel
     def reset_kernel(self, x:ti.ext_arr()):

plb/engine/primitive/primive_base.py
@@ -129,26 +129,58 @@ class Primitive:
     @ti.kernel
     def get_state_kernel(self, f: ti.i32, controller: ti.ext_arr()):
         for j in ti.static(range(3)):
-            controller[j] = self.position[f][j]
+            controller[j*3] = self.position[f][j]
+            controller[j*3+1] = self.v[f][j]
+            controller[j*3+2] = self.w[f][j]
         for j in ti.static(range(4)):
-            controller[j+self.dim] = self.rotation[f][j]
+            controller[j+self.dim*3] = self.rotation[f][j]
+
+    @ti.kernel
+    def get_state_grad_kernel(self,f:ti.i32, controller_grad: ti.ext_arr()):
+        for j in ti.static(range(3)):
+            controller_grad[j*3] = self.position.grad[f][j]
+            controller_grad[j*3+1] = self.v.grad[f][j]
+            controller_grad[j*3+2] = self.w.grad[f][j]
+        for j in ti.static(range(4)):
+            controller_grad[j+self.dim*3] = self.rotation.grad[f][j]
 
     @ti.kernel
     def set_state_kernel(self, f: ti.i32, controller: ti.ext_arr()):
         for j in ti.static(range(3)):
-            self.position[f][j] = controller[j]
+            self.position[f][j] = controller[j*3]
+            self.v[f][j] = controller[j*3+1]
+            self.w[f][j] = controller[j*3+2]
         for j in ti.static(range(4)):
-            self.rotation[f][j] = controller[j+self.dim]
+            self.rotation[f][j] = controller[j+self.dim*3]
+
+    @ti.kernel
+    def set_state_grad_kernel(self,f:ti.i32, controller: ti.ext_arr()):
+        for j in ti.static(range(3)):
+            self.position.grad[f][j] = controller[j*3]
+            self.v.grad[f][j] = controller[j*3+1]
+            self.w.grad[f][j] = controller[j*3+2]
+        for j in ti.static(range(4)):
+            self.rotation.grad[f][j] = controller[j+self.dim*3]
 
     def get_state(self, f):
-        out = np.zeros((7), dtype=np.float64)
+        out = np.zeros((13), dtype=np.float64)
         self.get_state_kernel(f, out)
+        return out
+
+    def get_state_grad(self,f):
+        out = np.zeros((13), dtype=np.float64)
+        self.get_state_grad_kernel(f,out)
         return out
 
     def set_state(self, f, state):
         ss = self.get_state(f)
         ss[:len(state)] = state
         self.set_state_kernel(f, ss)
+
+    def set_state_grad(self,f,state_grad):
+        ss_grad = self.get_state_grad(f)
+        ss_grad[:len(state_grad)] = state_grad
+        self.set_state_kernel(f,ss_grad)
 
     @property
     def init_state(self):

plb/engine/taichi_layer.py
@@ -1,12 +1,40 @@
 from plb.engine.taichi_env import TaichiEnv
 import torch
 
+# We don't know number of particles and number of primitives
+# We do know each primitive has a 13-D vector as state
+# Assume state is a huge vector
+class StateFormat:
+    @staticmethod
+    def state2frame(state,n_particles,n_primitives):
+        x = state[:3*n_particles].view(n_particles,3).numpy()
+        v = state[3*n_particles:6*n_particles].view(n_particles,3).numpy()
+        F = state[6*n_particles:15*n_particles].view(n_particles,3,3).numpy()
+        C = state[15*n_particles:24*n_particles].view(n_particles,3,3).numpy()
+        frame = [x,v,F,C]
+        base = 24*n_particles
+        for i in range(n_primitives):
+            frame.append(state[base+i*13:base+(i+1)*13].numpy())
+        return frame
+
+    def frame2state(frame,n_particles,n_primitives):
+        state = torch.zeros(24*n_particles+13*n_primitives).double()
+        state[:3*n_particles] = torch.from_numpy(frame[0].flatten())
+        state[3*n_particles:6*n_particles] = torch.from_numpy(frame[1].flatten())
+        state[6*n_particles:15*n_particles] = torch.from_numpy(frame[2].flatten())
+        state[15*n_particles:24*n_particles] = torch.from_numpy(frame[3].flatten())
+        base = 24*n_particles
+        for i in range(len(frame)-4):
+            state[base+i*13:base+(i+1)*13] = torch.from_numpy(frame[i+4])
+        return state
+
 
 class TaichiLayer(torch.autograd.Function):
     @staticmethod
     def forward(ctx, taichi_env: TaichiEnv, state, action, cur=None):
         start_cur = taichi_env.simulator.cur if cur == None else cur
-        taichi_env.set_state(state.numpy(),taichi_env.softness,False,start_cur)
+        frame = StateFormat.state2frame(state)
+        taichi_env.set_state(frame,taichi_env.softness,False,start_cur)
         taichi_env.step(action=action.numpy())
         end_cur = taichi_env.simulator.cur # Point to the last frame in this step
         next_state = taichi_env.get_current_state()['state']
@@ -16,8 +44,11 @@ class TaichiLayer(torch.autograd.Function):
     @staticmethod
     def backward(ctx,next_state_grad):
         start_cur,end_cur,taichi_env = ctx.saved_tensors
-        taichi_env.set_state_grad(end_cur,next_state_grad.numpy())
-        taichi_env.backprop(end_cur,start_cur) # @David
-        state_grad = taichi_env.get_state_grad(start_cur) # Get gradient for ALL state
+        grad_frame = StateFormat.state2frame(next_state_grad)
+        taichi_env.set_state_grad(end_cur,grad_frame)
+        taichi_env.backprop(end_cur,start_cur)
+        frame_grad = taichi_env.get_state_grad(start_cur) # Get gradient for ALL state
         action_grad = taichi_env.get_action_grad(start_cur)
+        action_grad = torch.from_numpy(action_grad)
+        state_grad = StateFormat.frame2state(frame_grad)
         return state_grad,action_grad