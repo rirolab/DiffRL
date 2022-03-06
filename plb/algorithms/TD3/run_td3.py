395ef2971f8ed5d2c67f2b17d0c81b59e2045943
EE-LiuYunhao <liuyunhao1578@gmail.com>
2022-03-06 14:49:44 +0000

    add type inference

--- plb/algorithms/TD3/run_td3.py
+++ plb/algorithms/TD3/run_td3.py
@@ -53,13 +53,13 @@ def parse_argument(externalArgs):
     parser.add_argument("--seed", default=0, type=int)              # Sets Gym, PyTorch and Numpy seeds
     parser.add_argument("--start_timesteps", default=2500, type=int)# Time steps initial random policy is used
     parser.add_argument("--eval_freq", default=200, type=int)       # How often (time steps) we evaluate
-    parser.add_argument("--max_timesteps", default=500000, type=int)   # Max time steps to run environment
-    parser.add_argument("--expl_noise", default=0.02) # 0.1                # Std of Gaussian exploration noise
+    parser.add_argument("--max_timesteps", default=500000, type=int)# Max time steps to run environment
+    parser.add_argument("--expl_noise", default=0.02, type=float)   # Std of Gaussian exploration noise
     parser.add_argument("--batch_size", default=256, type=int)      # Batch size for both actor and critic
-    parser.add_argument("--gamma", default=0.99, type=float)                 # Discount factor
-    parser.add_argument("--tau", default=0.001)                     # Target network update rate
-    parser.add_argument("--policy_noise", default=0.2) # 0.2              # Noise added to target policy during critic update
-    parser.add_argument("--noise_clip", default=0.5)                # Range to clip target policy noise
+    parser.add_argument("--gamma", default=0.99, type=float)        # Discount factor
+    parser.add_argument("--tau", default=0.001, type=float)         # Target network update rate
+    parser.add_argument("--policy_noise", default=0.2, type=float)  # Noise added to target policy during critic update
+    parser.add_argument("--noise_clip", default=0.5, type=float)    # Range to clip target policy noise
     parser.add_argument("--policy_freq", default=2, type=int)       # Frequency of delayed policy updates
     parser.add_argument("--save_model", action="store_true")        # Save model and optimizer parameters
     parser.add_argument("--load_model", default="")                 # Model load file name, "" doesn't load, "default" uses file_name
